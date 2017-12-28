846bb656432175af4135ae1fa329a3bdbc6ec04e
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2017-12-29 00:22:24 +0300

    Remove `unescapeIdentifier` from typescriptServices.ts.internal.kt
    since it's no longer internal

--- src/declarations/TypescriptServices/typescriptServices.ts.internal.kt
+++ src/declarations/TypescriptServices/typescriptServices.ts.internal.kt
@@ -3,6 +3,5 @@ package typescriptServices.ts
 
 // Declarations that declared inside namespace marked as internal and not exist inside typescriptServices.d.ts and typescript.d.ts, but available at runtime
 
-external fun unescapeIdentifier(identifier: String): String
 external fun normalizePath(path: String): String
 external fun getDirectoryPath(path: String): String
