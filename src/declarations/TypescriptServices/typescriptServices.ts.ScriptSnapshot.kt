0987bad8f893c6e8d49e9d43a642211e4f626417
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2017-12-29 00:22:24 +0300

    Migration to new tsc: add missed imports

--- src/declarations/TypescriptServices/typescriptServices.ts.ScriptSnapshot.kt
+++ src/declarations/TypescriptServices/typescriptServices.ts.ScriptSnapshot.kt
@@ -17,5 +17,6 @@ import org.w3c.notifications.*
 import org.w3c.performance.*
 import org.w3c.workers.*
 import org.w3c.xhr.*
+import typescriptServices.ts.IScriptSnapshot
 
 external fun fromString(text: String): IScriptSnapshot = definedExternally
