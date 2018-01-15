aa3a8f2b46dc88c182778beed1f51d216006ba3f
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2018-01-15 20:39:43 +0300

    Rename "Headers" to "MyHeaders" to avoid clash with declaration from core.d.ts

--- testData/typeAlias/typeParams.d.ts
+++ testData/typeAlias/typeParams.d.ts
@@ -5,7 +5,7 @@ interface List<T> {
 
 type Values<V> = List<V>;
 type MultiMap<K,V> = Map<K,Values<V>>;
-type Headers = MultiMap<String,String>
+type MyHeaders = MultiMap<String,String>
 type Ref<T> = string | ((instance: T) => any);
 
 declare var fooMap: MultiMap<string,number>;
@@ -20,11 +20,11 @@ declare var listOfStringOrNumber: string | List<string | number>;
 
 declare function listOfNumberOrString(a: List<number | String>);
 
-declare var headers: Headers;
+declare var headers: MyHeaders;
 
-declare function getHeaders(): Headers;
+declare function getHeaders(): MyHeaders;
 
-declare function addHeaders(headers: Headers);
+declare function addHeaders(headers: MyHeaders);
 
 declare var someRef: Ref<number>;
 
