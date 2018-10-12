5d48c0eac619c1907add6fc39aea1a044b845ec7
Eric Pabst <epabst@gmail.com>
2018-10-11 23:20:21 -0600

    Fix compile errors with generic optional methods
    Fixes https://github.com/Kotlin/ts2kt/issues/110

--- testData/interface/generics/genericOptionalMethods.d.ts
+++ testData/interface/generics/genericOptionalMethods.d.ts
@@ -2,4 +2,5 @@ interface Foo<T> {
     methodWithOutArgs?();
     methodWithString?<A>(s: A): T;
     methodWithManyArgs?<A extends T, B>(n: A, settings: Bar): B;
+    methodWithInOutArg?<A extends T>(n: A): A;
 }
